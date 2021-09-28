1492c1993f7ee096e8620e12c7914514c5765404
Brian Ellis <brian@briansresume.com>
2021-09-28 16:06:40 -0400

    added .internal folder

---
+++ src/Classes/Math/add.ts
@@ -0,0 +1,3 @@
+const add = createMathOperation((augend, addend) => augend + addend, 0)
+
+export default add
--- src/Classes/String/Encoding.ts
+++
@@ -1,8 +0,0 @@
-
-const matchCnt  = ( str: string , pattern: RegExp ) => {
-  const MATCH = ( str || '' ).match( pattern );
-  return MATCH ? MATCH.length : 0;
-}
-const isASCII   = ( str: string ) => {
-  const REG_EX = /[\x00-\x7F]/;
-}
